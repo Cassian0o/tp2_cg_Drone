36944a42e1a42db6a4754126df42a1f4d53772bb
Thainá Cristina Martins Miranda Farias <104574273+ThainaCMartins@users.noreply.github.com>
2026-06-05 14:37:08 -0300

    Update Cockpit class to handle transparency
    Refactor texture loading and add transparency properties.

--- src/objects/Cockpit.js
+++ src/objects/Cockpit.js
@@ -5,16 +5,40 @@ export class Cockpit extends GameObject {
   constructor(gl, programInfo) {
     super();
     this.programInfo = programInfo;
-
     this.bufferInfo = twgl.primitives.createSphereBufferInfo(gl, 0.8, 16, 16);
 
-    AssetManager.loadTexture(
-      "metal_specular",
-      "assets/textures/metal_specular.jpg",
-    );
-
+    AssetManager.loadTexture("metal_specular", "assets/textures/metal_specular.jpg");
     this.texture = AssetManager.getTexture("metal_specular");
 
     this.position = [0, 0.8, -0.5];
+
+    // Sinaliza para ser desenhado por último
+    this.transparent = true;
+    this.opacity = 0.35;
+  }
+
+  draw(gl, globalUniforms) {
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+    gl.depthMask(false);
+
+    gl.useProgram(this.programInfo.program);
+    twgl.setBuffersAndAttributes(gl, this.programInfo, this.bufferInfo);
+
+    const world = this.getWorldMatrix();
+    twgl.setUniforms(this.programInfo, globalUniforms);
+    twgl.setUniforms(this.programInfo, {
+      u_world: world,
+      u_worldInverseTranspose: twgl.m4.transpose(twgl.m4.inverse(world)),
+      u_diffuseMap: this.texture,
+      u_opacity: this.opacity,
+    });
+
+    twgl.drawBufferInfo(gl, this.bufferInfo);
+
+    gl.depthMask(true);
+    gl.disable(gl.BLEND);
+
+    this.children.forEach((child) => child.draw(gl, globalUniforms));
   }
 }
